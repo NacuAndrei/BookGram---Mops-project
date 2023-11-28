48ae67466dab3de4eddd2d5802a867c2a7569f6c
Atudorei Miruna Gabriela <atudorei.miruna@yahoo.ro>
2023-11-28 19:39:00 +0200

    initial commit

---
+++ Backend/Proiect1.DAL/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Proiect1.DAL.Entities;
+
+namespace Proiect1.DAL
+{
+    public class AppDbContext : IdentityDbContext<
+        User,
+        Role,
+        int,
+        IdentityUserClaim<int>,
+        UserRole,
+        IdentityUserLogin<int>,
+        IdentityRoleClaim<int>,
+        IdentityUserToken<int>>
+
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
