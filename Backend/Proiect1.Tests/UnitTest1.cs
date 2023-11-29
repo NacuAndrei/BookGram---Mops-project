b9e3e15bd73bfbc0f295d800f5861637e32a0a29
satudoreimiruna <atudorei.miruna@yahoo.ro>
2023-11-29 17:22:55 +0200

    fix test project

--- Backend/Proiect1.Tests/UnitTest1.cs
+++ Backend/Proiect1.Tests/UnitTest1.cs
@@ -1,10 +1,11 @@
-namespace Proiect1.Tests;
-
-public class UnitTest1
+namespace Proiect1.Tests
 {
-    [Fact]
-    public void Test1()
+    public class UnitTest1
     {
+        [Fact]
+        public void Test1()
+        {
 
+        }
     }
 }
