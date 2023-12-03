ce204aa3689755637d47bd66e1b672d2491b1d9f
satudoreimiruna <atudorei.miruna@yahoo.ro>
2023-12-03 16:27:04 +0200

    add tests for book methods

--- Backend/Proiect1.BLL/Interfaces/IBookManager.cs
+++ Backend/Proiect1.BLL/Interfaces/IBookManager.cs
@@ -1,10 +1,6 @@
 ﻿using Proiect1.BLL.Models;
 using Proiect1.DAL.Entities;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Proiect1.BLL.Interfaces
 {
